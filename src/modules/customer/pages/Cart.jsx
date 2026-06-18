fe277314e4682d94842d25176507daaaf18e7d85
Khalid0226 <ansarikhalid8054@gmail.com>
2026-06-18 12:23:06 +0530

    new feature

--- src/modules/customer/pages/Cart.jsx
+++ src/modules/customer/pages/Cart.jsx
@@ -73,7 +73,7 @@ export default function Cart() {
                                 </div>
                                 
                                 <button className="w-full bg-slate-900 text-white py-4 rounded-xl font-black hover:bg-orange-500 transition-all shadow-lg shadow-orange-500/20">
-                                    Proceed to payment
+                                    Proceed to Checkout
                                 </button>
                             </div>
                         </div>
