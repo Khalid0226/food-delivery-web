4d87bcd4108c138ffe70b7aabe8108e118c7f2f8
Khalid0226 <ansarikhalid8054@gmail.com>
2026-08-17 11:14:04 +0530

    deploy

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -2,20 +2,21 @@ import axios from 'axios';
 
 // 1. Base URL set kar lo apne backend ka
 const API = axios.create({
-baseURL: 'http://localhost:2500/api', // Apne backend ka URL yahan daalna
+    // baseURL: 'http://localhost:2500/api', // Apne backend ka URL yahan daalna
+    baseURL: 'https://food-delivery-web-wsx1.onrender.com/api',
 });
 
 // 2. Interceptor: Yeh har API request jaane se pehle chalega
 API.interceptors.request.use((config) => {
-const token = localStorage.getItem('token'); // Login ke waqt jo token save kiya tha
+    const token = localStorage.getItem('token'); // Login ke waqt jo token save kiya tha
 
-if (token) {
-    config.headers.Authorization = `Bearer ${token}`; // Automatic header me lag jayega
-}
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`; // Automatic header me lag jayega
+    }
 
-return config;
+    return config;
 }, (error) => {
-return Promise.reject(error);
+    return Promise.reject(error);
 });
 
 export default API;
